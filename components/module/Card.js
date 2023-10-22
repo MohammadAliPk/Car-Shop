4e80e8239b5d22bb4f6e79d8d7617252565f82e7
Mohammad Ali <aliparkour83@gmail.com>
2023-10-22 21:28:05 +0330

    cards added to the project

---
+++ components/module/Card.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+// icons
+import Location from '../icons/Location'
+
+// styles
+import styles from './Card.module.css'
+import Link from 'next/link';
+
+
+const Card = (props) => {
+
+    const { id, category, image, description, location, model, price, name, year, distance } = props
+
+    return (
+        <Link href={`/cars/${id}`}>
+            <div className={styles.container}>
+                <img src={image} alt={name} className={styles.image} />
+                <h4 className={styles.title}>{`${name} ${model}`}</h4>
+                <p className={styles.detail}>{`${year} . ${distance}km`}</p>
+                <div className={styles.footer}>
+                    <p>$ {price}</p>
+                    <div className={styles.location}>
+                        <p>{location}</p>
+                        <Location />
+                    </div>
+                </div>
+            </div>
+        </Link>
+    );
+};
+
+export default Card;
--- components/templates/CarsPage.js
+++ components/templates/CarsPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import styles from './CarsPage.module.css'
+import Card from '../module/Card';
 
 const CarsPage = ({ data }) => {
     return (
         <div className={styles.container}>
-            {data.map(car => <p key={car.id}>{car.name}</p>)}
+            {data.map(car =>
+                <Card key={car.id} {...car} />
+            )}
         </div>
     );
 };
