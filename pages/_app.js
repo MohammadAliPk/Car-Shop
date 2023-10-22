8e2dc30f54a6552945d7be0a00279d18ed15603f
Mohammad Ali <aliparkour83@gmail.com>
2023-10-23 01:55:10 +0330

    type error fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 
-import Layout from '../components/layout/layout'
+import Layout from '../components/layout/Layout'
 
 export default function App({ Component, pageProps }) {
   return (
